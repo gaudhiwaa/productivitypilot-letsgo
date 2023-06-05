07c2032be15a6076fb40bd0531f459dee022ed3c
vivov15 <127943217+vivov15@users.noreply.github.com>
2023-06-05 17:22:16 +0800

    static design revision

--- Android/app/src/main/java/com/bangkit/productivitypilot/StatisticActivity.kt
+++ Android/app/src/main/java/com/bangkit/productivitypilot/StatisticActivity.kt
@@ -35,43 +35,45 @@ class StatisticActivity : AppCompatActivity() {
         val hourThu = findViewById<TextView>(R.id.hour_thu)
         val hourFri = findViewById<TextView>(R.id.hour_fri)
 
+
+
         hourMon.text = statisticData.hourMon
         hourTue.text = statisticData.hourTue
         hourWed.text = statisticData.hourWed
         hourThu.text = statisticData.hourThu
         hourFri.text = statisticData.hourFri
 
-//        bottomNavigationView = findViewById(R.id.bottomNavigationView)
-//        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-//            when (item.itemId) {
-//                R.id.menu_home -> {
-//                    // Handle home item selection
-//                    true
-//                }
-//                R.id.menu_search -> {
-//                    // Handle search item selection
-//                    true
-//                }
-//                R.id.menu_profile -> {
-//                    // Handle profile item selection
-//                    val intent = Intent(this, ProfileActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//                R.id.menu_studycamera -> {
-//                    // Handle study camera item selection
-//                    true
-//
-//                }
-//                R.id.menu_statistics -> {
-//                    // Handle statistics item selection
-//                    val intent = Intent(this, StatisticActivity::class.java)
-//                    startActivity(intent)
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                   // Handle home item selection
+                    true
+                }
+                R.id.menu_search -> {
+                    // Handle search item selection
+                    true
+                }
+                R.id.menu_profile -> {
+                    // Handle profile item selection
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+               }
+               R.id.menu_studycamera -> {
+                    // Handle study camera item selection
+                    true
+
+                }
+                R.id.menu_statistics -> {
+                    //Handle statistics item selection
+                    val intent = Intent(this, StatisticActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
 
