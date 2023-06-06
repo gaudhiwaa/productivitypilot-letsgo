e798a30ba9e7a9220af74885d8dce8f7655fd092
gaudhiwaa <gaudhiwaa@gmail.com>
2023-06-07 06:39:02 +0700

    fix: activity to branch

--- Android/app/src/main/java/com/bangkit/productivitypilot/ProfileFragment.kt
+++ Android/app/src/main/java/com/bangkit/productivitypilot/ProfileFragment.kt
@@ -1,26 +1,22 @@
 package com.bangkit.productivitypilot
+
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileFragment : Fragment() {
     private lateinit var progressBar: ProgressBar
     private lateinit var usernameTextView: TextView
     private lateinit var nameTextView: TextView
-    private lateinit var emailTextView: TextView
-    private lateinit var occupationTextView: TextView
-    private lateinit var institutionTextView: TextView
-    private lateinit var locationTextView: TextView
-    private lateinit var hobbyTextView: TextView
-    private lateinit var monthlyProductiveHourTextView: TextView
-    private lateinit var userPointTextView: TextView
-    private lateinit var userIdTextView: TextView
     private lateinit var progressMedalBar: ProgressBar
 
     private lateinit var auth: FirebaseAuth
@@ -28,31 +24,25 @@ class ProfileActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        progressBar = findViewById(R.id.progressBar)
-        usernameTextView = findViewById(R.id.usernameTextView)
-        nameTextView = findViewById(R.id.nameTextView)
-        progressMedalBar = findViewById(R.id.progressMedalBar)
+        progressBar = view.findViewById(R.id.progressBar)
+        usernameTextView = view.findViewById(R.id.usernameTextView)
+        nameTextView = view.findViewById(R.id.nameTextView)
+        progressMedalBar = view.findViewById(R.id.progressMedalBar)
 
         // Set the progress to 30%
         progressMedalBar.progress = 30
 
-//        emailTextView = findViewById(R.id.emailTextView)
-//        occupationTextView = findViewById(R.id.occupationTextView)
-//        institutionTextView = findViewById(R.id.institutionTextView)
-//        locationTextView = findViewById(R.id.locationTextView)
-//        hobbyTextView = findViewById(R.id.hobbyTextView)
-//        monthlyProductiveHourTextView = findViewById(R.id.monthlyProductiveHourTextView)
-//        userPointTextView = findViewById(R.id.userPointTextView)
-//        userIdTextView = findViewById(R.id.userIdTextView)
-
         auth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
 
-        val userId = intent.getStringExtra("userId") ?: ""
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity() as AppCompatActivity
+        activity.supportActionBar?.hide()
+
+        val userId = arguments?.getString("userId") ?: ""
 
         if (userId.isNotEmpty()) {
             progressBar.visibility = View.VISIBLE // Show the progress bar
@@ -64,24 +54,9 @@ class ProfileActivity : AppCompatActivity() {
                     if (document != null && document.exists()) {
                         val username = document.getString("username")
                         val name = document.getString("name")
-                        val email = document.getString("email")
-                        val occupation = document.getString("occupation")
-                        val institution = document.getString("institution")
-                        val location = document.getString("location")
-                        val hobby = document.getString("hobby")
-                        val monthlyProductiveHour = document.getLong("monthlyProductiveHour")
-                        val userPoint = document.getLong("userPoint")
 
                         usernameTextView.text = "@$username"
                         nameTextView.text = "$name"
-//                        emailTextView.text = "Email: $email"
-//                        occupationTextView.text = "Occupation: $occupation"
-//                        institutionTextView.text = "Institution: $institution"
-//                        locationTextView.text = "Location: $location"
-//                        hobbyTextView.text = "Hobby: $hobby"
-//                        monthlyProductiveHourTextView.text = "Monthly Productive Hours: $monthlyProductiveHour"
-//                        userPointTextView.text = "User Points: $userPoint"
-//                        userIdTextView.text = "User ID: $userId"
 
                         progressBar.visibility = View.GONE // Hide the progress bar
                     }
@@ -90,11 +65,9 @@ class ProfileActivity : AppCompatActivity() {
                     // Handle failure
                     progressBar.visibility = View.GONE // Hide the progress bar
                 }
-
-
         }
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView = view.findViewById(R.id.bottomNavigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_home -> {
@@ -112,23 +85,19 @@ class ProfileActivity : AppCompatActivity() {
                 R.id.menu_studycamera -> {
                     // Handle study camera item selection
                     true
-
                 }
                 R.id.menu_statistics -> {
                     // Handle statistics item selection
-                    val intent = Intent(this, StatisticActivity::class.java)
-                    startActivity(intent)
+                    val fragment = StatisticFragment()
+                    requireActivity().supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit()
                     true
                 }
                 else -> false
             }
         }
+
+        return view
     }
-
-
-
-
-
 }
-
-
--- Android/app/src/main/java/com/bangkit/productivitypilot/StatisticFragment.kt
+++ Android/app/src/main/java/com/bangkit/productivitypilot/StatisticFragment.kt
@@ -1,8 +1,6 @@
 package com.bangkit.productivitypilot
-
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,14 +8,16 @@ import android.widget.TextView
 import com.bangkit.productivitypilot.model.StatisticData
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class StatisticActivity : AppCompatActivity() {
+class StatisticFragment : Fragment() {
 
     private lateinit var statisticData: StatisticData
-    private lateinit var bottomNavigationView: BottomNavigationView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_statistic)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_statistic, container, false)
 
         // Initialize the StatisticData object
         statisticData = StatisticData(
@@ -29,13 +29,11 @@ class StatisticActivity : AppCompatActivity() {
         )
 
         // Initialize TextViews and display the values from statisticData
-        val hourMon = findViewById<TextView>(R.id.hour_mon)
-        val hourTue = findViewById<TextView>(R.id.hour_tue)
-        val hourWed = findViewById<TextView>(R.id.hour_wed)
-        val hourThu = findViewById<TextView>(R.id.hour_thu)
-        val hourFri = findViewById<TextView>(R.id.hour_fri)
-
-
+        val hourMon = view.findViewById<TextView>(R.id.hour_mon)
+        val hourTue = view.findViewById<TextView>(R.id.hour_tue)
+        val hourWed = view.findViewById<TextView>(R.id.hour_wed)
+        val hourThu = view.findViewById<TextView>(R.id.hour_thu)
+        val hourFri = view.findViewById<TextView>(R.id.hour_fri)
 
         hourMon.text = statisticData.hourMon
         hourTue.text = statisticData.hourTue
@@ -43,11 +41,11 @@ class StatisticActivity : AppCompatActivity() {
         hourThu.text = statisticData.hourThu
         hourFri.text = statisticData.hourFri
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+        val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView?.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_home -> {
-                   // Handle home item selection
+                    // Handle home item selection
                     true
                 }
                 R.id.menu_search -> {
@@ -56,26 +54,28 @@ class StatisticActivity : AppCompatActivity() {
                 }
                 R.id.menu_profile -> {
                     // Handle profile item selection
-                    val intent = Intent(this, ProfileFragment::class.java)
-                    startActivity(intent)
+                    // Replace `YourProfileFragment` with the actual fragment you want to navigate to
+                    val fragment = Fragment()
+                    requireActivity().supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit()
                     true
-               }
-               R.id.menu_studycamera -> {
+                }
+                R.id.menu_studycamera -> {
                     // Handle study camera item selection
                     true
-
                 }
                 R.id.menu_statistics -> {
-                    //Handle statistics item selection
-                    val intent = Intent(this, StatisticActivity::class.java)
-                    startActivity(intent)
+                    // Handle statistics item selection
+                    // No need to navigate since the current fragment is already the statistics fragment
                     true
                 }
                 else -> false
             }
         }
+
+        return view
     }
-
-
 }
+
 
